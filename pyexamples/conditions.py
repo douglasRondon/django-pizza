8be6c106933f40cac4f174cf4e3173106aab7243
douglasr <douglasrndn@gmail.com>
2015-08-18 09:41:48 -0300

    mais coisa

--- pyexamples/conditions.py
+++ pyexamples/conditions.py
@@ -1,1 +1,14 @@
 print('Hello World')
+
+sauce = 'tomato'
+order_description = """
+blablabla
+blablabla
+"""
+
+if sauce.startswith('toma'):
+	print("it's probably tomato..")
+else:
+	print("not tomato")
+if order_description:
+	print(order_description)
