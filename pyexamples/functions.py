7a3eff8dab4c219efeef4f2f8ff27ef49de21a05
douglasr <douglasrndn@gmail.com>
2015-08-18 11:39:48 -0300

    function2.0

--- pyexamples/functions.py
+++ pyexamples/functions.py
@@ -1,14 +1,20 @@
 pedidos = []
 
 
-def adiciona_pedido(nome, sabor, observacao='sem obs'):
+def adiciona_pedido(nome, sabor, observacao=None):
 	pedido = {}
 	pedido['nome'] = nome
 	pedido['sabor'] = sabor
-	pedido['observacao'] = observacao	
+	pedido['observacao'] = observacao
+	
 	return pedido
 
-print(pedidos)
+
 pedidos.append(adiciona_pedido('mario', 'pepperoni'))
-pedidos.append(adiciona_pedido('marco', 'presunto'))
-print( pedidos )
+pedidos.append(adiciona_pedido('marco', 'presunto','xyz'))
+for pedido in pedidos:
+	template = 'Nome: {nome}\nSabor: {sabor}'
+	print(template.format(**pedido))
+	if pedido['observacao']:
+		print('Observacao: {}'.format(pedido['observacao']))
+	print('-'*20)
