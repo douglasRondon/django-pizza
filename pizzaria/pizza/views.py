11c7bb11b349a4e5f8ac8ebd1283addc0864d906
douglasr <douglasrndn@gmail.com>
2015-08-18 17:18:22 -0300

    views

--- pizzaria/pizza/views.py
+++ pizzaria/pizza/views.py
@@ -2,10 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Pizza
+
+
 def menu(request):
 	pizzas = Pizza.objects.all()
-	result = ''
-	for pizza in pizzas:
-		result += pizza.name
-		result += '<br>'
-	return HttpResponse(result)
+	context = {'pizzas': pizzas}
+	return render(request,'pizza/menu.html', context)
